4b8e9d145db5879cc3eb6a8108a8e507f76d811e
Jakub Piskiewicz <jakub@piskiewicz.org>
2023-02-25 14:21:51 +0100

    Fixed fun call

--- modes/vterm/evil-collection-vterm.el
+++ modes/vterm/evil-collection-vterm.el
@@ -217,8 +217,8 @@ Save in REGISTER or in the kill-ring with YANK-HANDLER."
 ;; If more than one char is selected, replace the whole selection
 ;; with the char typed in after pushing the "r" key.
 (evil-define-operator evil-collection-vterm-replace (beg end)
-  (let ((text (filter-buffer-substring beg end)))
-    (message text))
+  (let (text (filter-buffer-substring beg end)))
+  (message text))
 
 (evil-define-motion evil-collection-vterm-next-line (count)
   "Move the cursor COUNT lines down.
