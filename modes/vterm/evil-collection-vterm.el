5463e57116ac7f46788d7765468242e9486148a6
Jakub Piskiewicz <jakub@piskiewicz.org>
2023-02-25 14:34:34 +0100

    type

--- modes/vterm/evil-collection-vterm.el
+++ modes/vterm/evil-collection-vterm.el
@@ -216,8 +216,8 @@ Save in REGISTER or in the kill-ring with YANK-HANDLER."
 ;; This emulates replace mode.
 ;; If more than one char is selected, replace the whole selection
 ;; with the char typed in after pushing the "r" key.
-(evil-define-operator evil-collection-vterm-replace (beg end)
-  (message "%s" (filter-buffer-substring beg end)))
+(evil-define-operator evil-collection-vterm-replace (beg end type)
+  (message "type: %s, value: %s" (filter-buffer-substring beg end) (type)))
 
 (evil-define-motion evil-collection-vterm-next-line (count)
   "Move the cursor COUNT lines down.
